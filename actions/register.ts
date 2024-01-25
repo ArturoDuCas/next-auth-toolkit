bd3182c208e95dff17a50bf4367c75904fcd508b
ArturoDuCas <a00833516@tec.mx>
2024-01-25 17:41:54 -0600

    feat: make sign up with credentials

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,39 @@
 "use server";
+
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+import {db} from "@/lib/db";
+
 import {RegisterSchema} from "@/schemas";
+import {getUserByEmail} from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
-  if(!validateFields.success) {
-    return { error: "Invalid fields!"}
+  if (!validateFields.success) { // If the validation fails, return an error
+    return {error: "Invalid fields!"}
   }
 
-  return { success: "Email sent!"}
+  const {email, password, name} = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) { // If the user already exists, return an error
+    return {error: "Email already in use!"}
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token via email
+
+
+  return {success: "User created!"}
 };
--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,9 @@ declare global {
   let prisma: PrismaClient | undefined;
 }
 
+// @ts-ignore
 export const db = globalThis.prisma || new PrismaClient();
 
-if(process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if(process.env.NODE_ENV !== "production") { // @ts-ignore
+  globalThis.prisma = db;
+}
